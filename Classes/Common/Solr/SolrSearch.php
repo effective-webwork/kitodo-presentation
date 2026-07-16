fedf412674371c952bf18bcc11081460ae094ed7
haogatyp <gatermann@effective-webwork.de>
2026-07-16 13:36:35 +0200

    Bugfix "No metadata for document with uid="

--- Classes/Common/Solr/SolrSearch.php
+++ Classes/Common/Solr/SolrSearch.php
@@ -567,6 +567,11 @@ class SolrSearch implements \Countable, \Iterator, \ArrayAccess, QueryResultInte
                             continue;
                         }
                         if (!empty($doc['page'])) {
+                            $metadataToplevel = $this->fetchToplevelMetadataFromSolr([
+                                'query' => 'toplevel:1 AND uid:' . $doc['uid'],
+                                'start' => 0,
+                                'rows' => 1,
+                            ]);
                             // it's probably a fulltext or metadata search
                             $searchResult = [];
                             $searchResult['page'] = $doc['page'];
@@ -603,6 +608,7 @@ class SolrSearch implements \Countable, \Iterator, \ArrayAccess, QueryResultInte
                                     $searchResult['metadata'][$indexName] = $doc['metadata'][$indexName];
                                 }
                             }
+                            $documents[$doc['uid']]['metadata'] = $metadataToplevel[$doc['uid']];
                             if (array_key_exists('fulltext', $this->searchParams) && $this->searchParams['fulltext'] == '1') {
                                 $searchResult['snippet'] = $doc['snippet'];
                                 $searchResult['highlight'] = $doc['highlight'];
